d07a38d935ff1ad9f5c970475a843a7bc2d13dda
michaelpierce <michael.todd.pierce@gmail.com>
2025-11-30 20:06:47 -0400

    Add comments to IProjectService.cs and ITagService.cs

--- Portfolio.Services/Project/IProjectService.cs
+++ Portfolio.Services/Project/IProjectService.cs
@@ -3,9 +3,42 @@ using Portfolio.Models.Project;
 
 namespace Portfolio.Services.Project;
 
+/// <summary>
+/// Defines the contract for a service that provides project-related operations.
+/// </summary>
 public interface IProjectService
 {
+    /// <summary>
+    /// Asynchronously retrieves a list of projects, with optional filtering by limit and tag IDs.
+    /// </summary>
+    /// <param name="limit">An optional parameter to limit the number of projects returned.</param>
+    /// <param name="tagIds">An optional array of tag IDs to filter the projects.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains
+    /// a <see cref="Result{T, TError}"/> object, where T is a list of <see cref="ProjectDto"/> 
+    /// and TError is an exception.
+    /// </returns>
     public Task<Result<List<ProjectDto>, Exception>> GetProjectsAsync(int? limit = null, int[]? tagIds = null);
+
+    /// <summary>
+    /// Asynchronously retrieves a project by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the project.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains
+    /// a <see cref="Result{T, TError}"/> object, where T is a <see cref="Models.Project.Project"/> 
+    /// and TError is an exception.
+    /// </returns>
     public Task<Result<Models.Project.Project, Exception>> GetProjectByIdAsync(string id);
+
+    /// <summary>
+    /// Asynchronously retrieves a project by its alias.
+    /// </summary>
+    /// <param name="alias">The alias of the project.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains
+    /// a <see cref="Result{T, TError}"/> object, where T is a <see cref="Models.Project.Project"/> 
+    /// and TError is an exception.
+    /// </returns>
     public Task<Result<Models.Project.Project, Exception>> GetProjectByAliasAsync(string alias);
 }
--- Portfolio.Services/Tag/ITagService.cs
+++ Portfolio.Services/Tag/ITagService.cs
@@ -2,8 +2,29 @@ using Portfolio.Models;
 
 namespace Portfolio.Services.Tag;
 
+/// <summary>
+/// Defines the contract for a service that provides tag-related operations.
+/// </summary>
 public interface ITagService
 {
+    /// <summary>
+    /// Asynchronously retrieves a list of tags.
+    /// </summary>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains
+    /// a <see cref="Result{T, TError}"/> object, where T is a list of <see cref="Models.Tag.Tag"/>
+    /// and TError is an exception.
+    /// </returns>
     public Task<Result<List<Models.Tag.Tag>, Exception>> GetTagsAsync();
+
+    /// <summary>
+    /// Asynchronously retrieves a tag by its unique identifier.
+    /// </summary>
+    /// <param name="tagId">The unique identifier of the tag.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation. The task result contains
+    /// a <see cref="Result{T, TError}"/> object, where T is a <see cref="Models.Tag.Tag"/>
+    /// and TError is an exception.
+    /// </returns>
     public Task<Result<Models.Tag.Tag, Exception>> GetTagByIdAsync(int tagId);
 }
